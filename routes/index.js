c9e6349de2a7fda58e5152fc0ed107749c5fc67f
rhernandez1996 <112348988+rhernandez1996@users.noreply.github.com>
2022-11-05 12:14:14 -0700

    push 2

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const auth = require('http-auth');
 const router = express.Router();
+const Registration = mongoose.model('Registration');
 const { check, validationResult } = require('express-validator');
+
+const basic = auth.basic({
+    file: path.join(_dirname, '../users.htpasswd'),
+});
 
 router.get('/', function(req,res) {
     res.render('form', { title: 'Registration form' });
 });
+
+router.get('/registrations', basic.check((req,res => {
+    Resigistration.find()
+      .then((registrations) => {
+  res.render('index', {title: 'Listening registrations', registrations });
+      })
+      .catch(() => { res.send('Sorry! Something went wrong.'); });
+})));
 
 router.post('/',
     [
@@ -19,7 +35,13 @@ function(req,res) {
     //console.log(req.body);
     const errors = validationResult(req);
     if (errors.isEmpty()) {
-        res.send('Thank you for your registration!');
+        const registration = new Registration(req.body);
+        registration.save()
+          .then(() => { res.send('Thank you for your registration!'); })
+          .catch((err) => {
+            console.log(err);
+            res.send('Sorry! Something went wrong.');
+          })
     } else {
        res.render("form", { 
         title: "Registration form", 
--- start.js
+++ start.js
@@ -1,6 +1,19 @@
-const app = require('./app');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-const server = app.listen(3000, function() {
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+mongoose.connection
+  .on('open', () => {
+    console.log('Mongoose connection open');
+  })
+
+require('./models/Registration');
+const app = require('./app');
+const server = app.listen(3000, () => {
     console.log(`Express is running on port ${server.address().port}`);
 }); 
 
